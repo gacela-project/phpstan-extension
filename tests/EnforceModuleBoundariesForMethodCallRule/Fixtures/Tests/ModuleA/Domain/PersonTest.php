f75e814e7e189523ea7c9e2e1d9b58fa48f7e747
Chemaclass <chemaclass@outlook.es>
2022-12-10 12:39:09 +0100

    Allow excluded callingCode

---
+++ tests/EnforceModuleBoundariesForMethodCallRule/Fixtures/Tests/ModuleA/Domain/PersonTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GacelaProject\PhpstanExtension\Tests\EnforceModuleBoundariesForMethodCallRule\Fixtures\Tests\ModuleA\Domain;
+
+use GacelaProject\PhpstanExtension\Tests\EnforceModuleBoundariesForMethodCallRule\Fixtures\ModuleA\Domain\Person;
+use GacelaProject\PhpstanExtension\Tests\EnforceModuleBoundariesForMethodCallRule\Fixtures\ModuleB\ModuleBFacadeInterface;
+use PHPUnit\Framework\TestCase;
+
+final class PersonTest extends TestCase
+{
+    public function test_something(): void
+    {
+        $p = new Person(
+            $this->createStub(ModuleBFacadeInterface::class)
+        );
+
+        self::assertEquals(get_class($p), $p->asString());
+    }
+}
